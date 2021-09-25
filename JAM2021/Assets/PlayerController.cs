da9d29124fe7bfd5e0027c148e4bbb1a06140cce
Will7461 <43720307+Will7461@users.noreply.github.com>
2021-09-25 19:11:37 +0200

    Movimiento de jugador

--- JAM2021/Assets/PlayerController.cs
+++ JAM2021/Assets/PlayerController.cs
@@ -55,24 +55,22 @@ public class PlayerController : MonoBehaviour
     public bool IsDead { get; private set; }
     public bool IsCrouching { get; private set; }
 
-    PlayerInput m_InputHandler;
-    CharacterController m_Controller;
-    Vector3 m_GroundNormal;
-    Vector3 m_CharacterVelocity;
-    float m_LastTimeJumped = 0f;
-    float m_CameraVerticalAngle = 0f;
-    float m_TargetCharacterHeight;
+    PlayerInput inputHandler;
+    CharacterController controller;
+    Vector3 groundNormal;
+    float lastTimeJumped = 0f;
+    float cameraVerticalAngle = 0f;
 
-    const float k_JumpGroundingPreventionTime = 0.2f;
-    const float k_GroundCheckDistanceInAir = 0.07f;
+    const float jumpGroundingPreventionTime = 0.2f;
+    const float groundCheckDistanceInAir = 0.07f;
 
     void Start()
     {
-        m_Controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
 
-        m_InputHandler = GetComponent<PlayerInput>();
+        inputHandler = GetComponent<PlayerInput>();
 
-        m_Controller.enableOverlapRecovery = true;
+        controller.enableOverlapRecovery = true;
     }
 
     void Update()
@@ -94,43 +92,135 @@ public class PlayerController : MonoBehaviour
     void GroundCheck()
     {
         //Asegurarse de que la distancia para comprobar el suelo es suficientemente pequeña para evitar snapping al suelo
-        float chosenGroundCheckDistance = IsGrounded ? (m_Controller.skinWidth + groundCheckDistance) : k_GroundCheckDistanceInAir;
+        float chosenGroundCheckDistance = IsGrounded ? (controller.skinWidth + groundCheckDistance) : groundCheckDistanceInAir;
 
         //Resetear valores antes de comprobar el suelo
         IsGrounded = false;
-        m_GroundNormal = Vector3.up;
+        groundNormal = Vector3.up;
 
         //Solo se realiza la comprobacion si ha pasado cierto tiempo desde el ultimo salto, para evitar que se snapee al suelo nada mas saltar
-        if (Time.time >= m_LastTimeJumped + k_JumpGroundingPreventionTime)
+        if (Time.time >= lastTimeJumped + jumpGroundingPreventionTime)
         {
             //Casteamos la capsula de comprobacion del suelo
-            Vector3 capsuleBottomHalf = transform.position + (transform.up * m_Controller.radius);
-            Vector3 capsuleTopHalf = transform.position + (transform.up * (m_Controller.height - m_Controller.radius));
+            Vector3 capsuleBottomHalf = transform.position + (transform.up * controller.radius);
+            Vector3 capsuleTopHalf = transform.position + (transform.up * (controller.height - controller.radius));
 
-            if (Physics.CapsuleCast(capsuleBottomHalf, capsuleTopHalf, m_Controller.radius, Vector3.down, out RaycastHit hit,
+            if (Physics.CapsuleCast(capsuleBottomHalf, capsuleTopHalf, controller.radius, Vector3.down, out RaycastHit hit,
                 chosenGroundCheckDistance, groundCheckLayers, QueryTriggerInteraction.Ignore))
             {
-                m_GroundNormal = hit.normal;
+                groundNormal = hit.normal;
 
                 //Si la normal del suelo es la misma que el vector hacia arriba del jugador, se considera un golpe con el suelo
                 //Y si el angulo de pendiente de ese suelo es menor que el del character controller
-                if (Vector3.Dot(hit.normal, transform.up) > 0f && (Vector3.Angle(transform.up, m_GroundNormal) <= m_Controller.slopeLimit))
+                if (Vector3.Dot(hit.normal, transform.up) > 0f && (Vector3.Angle(transform.up, groundNormal) <= controller.slopeLimit))
                 {
                     IsGrounded = true;
 
                     //Gestion de snapping con el suelo
-                    if (hit.distance > m_Controller.skinWidth)
+                    if (hit.distance > controller.skinWidth)
                     {
-                        m_Controller.Move(Vector3.down * hit.distance);
+                        controller.Move(Vector3.down * hit.distance);
                     }
                 }
             }
         }
     }
 
-    void HandleCharacterMovement()
+    public Vector3 GetDirectionReorientedOnSlope(Vector3 direction, Vector3 slopeNormal)
     {
-
+        Vector3 directionRight = Vector3.Cross(direction, transform.up);
+        return Vector3.Cross(slopeNormal, directionRight).normalized;
     }
 
+    void HandleCharacterMovement()
+    {
+        //Rotacion horizontal
+        {
+            //Rotar el transform con el input en su eje Y
+            transform.Rotate(new Vector3(0f, (inputHandler.GetLookInputsHorizontal() * rotationSpeed), 0f), Space.Self);
+        }
+
+        //Rotacion vertical
+        {
+            //Suma el input a la camara
+            cameraVerticalAngle += inputHandler.GetLookInputsVertical() * rotationSpeed;
+
+            // limita el movimiento vertical de la camara
+            cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -89f, 89f);
+
+            //Aplica el angulo vertical de forma local a la camara para que pivotee
+            playerCamera.transform.localEulerAngles = new Vector3(cameraVerticalAngle, 0, 0);
+        }
+
+        //Movimiento del player
+        bool isSprinting = inputHandler.GetSprintInputHeld();
+        {
+
+            float speedModifier = isSprinting ? sprintSpeedModifier : 1f;
+
+            //Convierte el input de movimiento a worldspace basado en la orientacion del player transform
+            Vector3 worldspaceMoveInput = transform.TransformVector(inputHandler.GetMoveInput());
+            Debug.Log(IsGrounded);
+            //Gestion de tocar el suelo
+            if (IsGrounded)
+            {
+                //Calcular velocidad deseada basandose en inputs, velocidad maxima y slope
+                Vector3 targetVelocity = worldspaceMoveInput * maxSpeedOnGround * speedModifier;
+
+                targetVelocity = GetDirectionReorientedOnSlope(targetVelocity.normalized, groundNormal) * targetVelocity.magnitude;
+
+                //Interpolacion entre la velocidad actual y la deseada, basandose en la aceleracion
+                CharacterVelocity = Vector3.Lerp(CharacterVelocity, targetVelocity, movementSharpnessOnGround * Time.deltaTime);
+
+                //Salto
+                if (IsGrounded && inputHandler.GetJumpInputDown())
+                {
+                    //Reseteo de velocidad en eje Y
+                    CharacterVelocity = new Vector3(CharacterVelocity.x, 0f, CharacterVelocity.z);
+
+                    //Se añade la fuerza vertical de salto
+                    CharacterVelocity += Vector3.up * jumpForce;
+
+                    // play sound
+                    //AudioSource.PlayOneShot(JumpSfx);
+
+                    //Se usara luego para prevenir snapping
+                    lastTimeJumped = Time.time;
+                    HasJumpedThisFrame = true;
+
+                    //Reseteo de valores
+                    IsGrounded = false;
+                    groundNormal = Vector3.up;
+                }
+
+            }
+            //Movimiento en el aire
+            else
+            {
+                //Sumar la acceleracion en el aire
+                CharacterVelocity += worldspaceMoveInput * accelerationSpeedInAir * Time.deltaTime;
+
+                //Limitar la velocidad en el aire
+                float verticalVelocity = CharacterVelocity.y;
+                Vector3 horizontalVelocity = Vector3.ProjectOnPlane(CharacterVelocity, Vector3.up);
+                horizontalVelocity = Vector3.ClampMagnitude(horizontalVelocity, maxSpeedInAir * speedModifier);
+                CharacterVelocity = horizontalVelocity + (Vector3.up * verticalVelocity);
+
+                //Aplicar la gravedad
+                CharacterVelocity += Vector3.down * gravityDownForce * Time.deltaTime;
+            }
+        }
+
+        //Aplicar la velocidad final como al CharacterController
+        Vector3 capsuleBottomBeforeMove = transform.position + (transform.up * controller.radius);
+        Vector3 capsuleTopBeforeMove = transform.position + (transform.up * (controller.height - controller.radius));
+        controller.Move(CharacterVelocity * Time.deltaTime);
+
+        //Detectar obstrucciones para ajustar la velocidad acordemente
+        if (Physics.CapsuleCast(capsuleBottomBeforeMove, capsuleTopBeforeMove, controller.radius, CharacterVelocity.normalized, 
+            out RaycastHit hit, CharacterVelocity.magnitude * Time.deltaTime, -1, QueryTriggerInteraction.Ignore))
+        {
+            CharacterVelocity = Vector3.ProjectOnPlane(CharacterVelocity, hit.normal);
+        }
+    }
 }
