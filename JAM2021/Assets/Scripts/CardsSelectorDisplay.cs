3d2d48ad3df75d11757a2e454ea69ccf906f209f
Starklosch <45690579+Starklosch@users.noreply.github.com>
2021-09-26 23:35:28 -0300

    Hands added

--- JAM2021/Assets/Scripts/CardsSelectorDisplay.cs
+++ JAM2021/Assets/Scripts/CardsSelectorDisplay.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CardsSelectorDisplay : MonoBehaviour
 {
-    GameObject cursorOb;
+    [SerializeField] GameObject cursorOb;
+
+    RectTransform cursorTransform;
+
     void Start()
     {
-        cursorOb = transform.GetChild(3).gameObject;
+        //cursorOb = transform.GetChild(3).gameObject;
+
+        cursorTransform = cursorOb.GetComponent<RectTransform>();
     }
 
     public void UpdateCursorUI(int index)
     {
         if (cursorOb != null)
         {
-            cursorOb.transform.position = new Vector3(transform.GetChild(index).position.x, cursorOb.transform.position.y, 0);
+            cursorOb.transform.SetParent(transform.GetChild(index));
+            cursorTransform.anchoredPosition = Vector3.zero;
+            //cursorOb.transform.position = new Vector3(transform.GetChild(index).position.x, cursorOb.transform.position.y, 0);
         }
     }
 }
